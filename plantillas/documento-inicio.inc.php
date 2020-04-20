ff48aa68da8bf336b3ed4b6c873a3e5a3c5e9748
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 16:22:30 -0600

    Mandamos a llamar a los archivos de css y js en nuestro sitio web

--- plantillas/documento-inicio.inc.php
+++ plantillas/documento-inicio.inc.php
@@ -14,9 +14,7 @@
         <meta name="description" content="Ayuda para los motores de busqueda como goole.com">
         
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="#">
-         
+        <link rel="stylesheet" type="text/css" href="<?php echo RUTA_CSS; ?>bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo RUTA_CSS; ?>estilos.css">
     </head>
     <body>
