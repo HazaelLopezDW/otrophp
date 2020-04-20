f32bc77a8d244dec0bcfc9a46a71ebeafe79d26b
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 10:58:15 -0600

    Agregamos la cabecera del sitio Web

--- plantillas/documento-inicio.inc.php
+++ plantillas/documento-inicio.inc.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <?php
+            if(!isset($titulo) || empty($titulo)){
+                $titulo = "Blog De JavaDevOne";
+            }
+            echo "<title>$titulo</title>";
+        ?>
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+        <meta name="description" content="Ayuda para los motores de busqueda como goole.com">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="#">
+         
+    </head>
+    <body>
