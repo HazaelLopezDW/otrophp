968b62cc5cf3924a3f5abae3d0e015b9a8a58a23
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 11:48:48 -0600

    Agregamos los datos para la conexion a nuestra base de datos y las rutas del sitio web

--- app/Config.inc.php
+++ app/Config.inc.php
@@ -1,1 +1,20 @@
-
+<?php
+    // Datos para hacer la conexion a la BD
+    define("NOMBRE_SERVIDOR", "localhost");
+    define("NOMBRE_USUARIO", "root");
+    define("PASSWORD", "");
+    define("NOMBRE_BD", "blog");
+    
+    // Rutas de nuestro sitio web
+    define("SERVIDOR", "http://localhost/otrophp/");
+    define("RUTA_ENTRADAS", SERVIDOR . "entradas");
+    define("RUTA_FAVORITOS", SERVIDOR . "favoritos");
+    define("RUTA_AUTORES", SERVIDOR . "autores");
+    define("RUTA_USER", SERVIDOR . "usuarios");
+    define("RUTA_LOGIN", SERVIDOR . "login");
+    define("RUTA_REGISTRO", SERVIDOR . "registro");
+    define("RUTA_REGISTRO_CORRECTO", SERVIDOR . "registro-correcto");
+    
+    
+    
+    
