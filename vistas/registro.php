f59b5903c7e49fe4d503cba6885ee1af3a6fea55
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 16:32:16 -0600

    Agregamos la cabecera y el cierre de nuestro sitio Web

--- vistas/registro.php
+++ vistas/registro.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Hola cruel mundo!!!!!";
+    include_once 'app/Config.inc.php';
+
+    $titulo = "Registro";
+
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+?>
+
+
+
+
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
 ?>
