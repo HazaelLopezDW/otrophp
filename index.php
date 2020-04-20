16c4a0fa8408ef91392d65866836d7e7d10047b7
Hazael Lopez <elhazael134@gmail.com>
2020-04-19 21:42:45 -0600

    Optenemos la ruta la explotamos la filtramos y limpiamos para crear nuestro enrutador del sitio

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-   echo "WALTER HAZAEL LÓPEZ DÍAZ";
+   
+    $ruta_completa = parse_url($_SERVER['REQUEST_URI']);
+    $ruta = $ruta_completa['path'];
+    
+    $partes_ruta = explode("/", $ruta);
+    $partes_ruta = array_filter($partes_ruta);
+    $partes_ruta = array_slice($partes_ruta, 0);
+    
+    echo '$partes_ruta[0] -> ' . $partes_ruta[0] . "<br>";
+    echo '$partes_ruta[1] -> ' . $partes_ruta[1] . "<br>";
+    echo '$partes_ruta[2] -> ' . $partes_ruta[2] . "<br>";
+    
+    if(count($partes_ruta) == 1){
+        if($partes_ruta[0] == "otrophp"){
+            include_once "vistas/home.php";
+        }
+    }
