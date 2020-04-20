d3917ba823450b1246eeb140268e00b61ebe6447
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 11:16:24 -0600

    Agregamos nuestra barra de navegacion

--- plantillas/navbar.inc.php
+++ plantillas/navbar.inc.php
@@ -1,1 +1,26 @@
-
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Menu de navegacion</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="<?php echo SERVIDOR; ?>" class="navbar-brand">JavaDevOne</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> Entradas</a></li>
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> Favoritos</a></li>
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> Autores</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> #</a></li>
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> Iniciar Sesión</a></li>
+                <li><a href="<?php echo RUTA_; ?>"><span class="glyphicon glyphicon-" data-hidden="true"></span> Registro</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
