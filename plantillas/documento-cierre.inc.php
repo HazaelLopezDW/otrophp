c58a219156ffefd4225e85e06baf7e4c63d38b45
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 16:31:34 -0600

    Corregimos el llamado a los archivos JavaScript de la carpeta js/

--- plantillas/documento-cierre.inc.php
+++ plantillas/documento-cierre.inc.php
@@ -1,6 +1,6 @@
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script type="text/javascript" src="<?php echo RUTA_JS; ?>/bootstrap.min.js"></script>
-        <script type="text/javascript" src="<?php echo RUTA_JS; ?>/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="<?php echo RUTA_JS; ?>jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="<?php echo RUTA_JS; ?>bootstrap.min.js"></script>
   </body>
 </html>
