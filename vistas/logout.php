a5d9388a4d6fbfbbd6fcb048752385e600c258bb
Hazael Lopez <elhazael134@gmail.com>
2020-04-20 16:47:38 -0600

    Agregamos la cabecera y el cierre a nuestras paginas login.php logout.php registro-correcto.php

--- vistas/logout.php
+++ vistas/logout.php
@@ -1,1 +1,16 @@
+<?php
+    include_once 'app/Config.inc.php';
 
+    $titulo = "Logout";
+
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+?>
+
+
+
+
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
+?>
